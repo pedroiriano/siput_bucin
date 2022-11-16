ec5c1a48239576fb88a32eea612f8b2302b3c9ce
ipiyuza <pdzeus83aw@gmail.com>
2022-11-16 23:55:32 +0700

    Fix Title

--- resources/views/includes/frontend-nav.blade.php
+++ resources/views/includes/frontend-nav.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar-->
 <nav class="navbar navbar-marketing navbar-expand-lg bg-transparent navbar-dark fixed-top">
     <div class="container px-5">
-        <a class="navbar-brand text-white" href="/">Siput Bucin SB</a>
+        <a class="navbar-brand text-white" href="/">Siput Bucin</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i data-feather="menu"></i></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto me-lg-5">
