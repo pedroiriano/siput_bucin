0a0dd30ddc0bf220615ede619227c532b5f6d19d
ipiyuza <pdzeus83aw@gmail.com>
2022-11-27 15:08:24 +0700

    Haki

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -35,25 +35,25 @@
             </div>
         </div>
         <div class="row gx-5 justify-content-center">
-            <div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
+            <div class="col-md-4 col-lg-3 mb-5 mb-lg-0">
                 <a class="card lift" href="{{ route('production') }}">
                     <img class="card-img-top" src="{{ asset('assets/images/informations/pirt-01.png') }}" alt="Gambar Kategori" />
                     <div class="card-body text-center py-3"><h6 class="card-title mb-0">PIRT</h6></div>
                 </a>
             </div>
-            <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
+            <div class="col-md-4 col-lg-3 mb-5 mb-md-0">
                 <a class="card lift" href="{{ route('halal') }}">
                     <img class="card-img-top" src="{{ asset('assets/images/informations/halal-01.png') }}" alt="Gambar Kategori" />
                     <div class="card-body text-center py-3"><h6 class="card-title mb-0">Sertifikasi Halal</h6></div>
                 </a>
             </div>
-            <div class="col-md-6 col-lg-4 mb-md-0">
+            <div class="col-md-4 col-lg-3 mb-md-0">
                 <a class="card lift" href="{{ route('market') }}">
                     <img class="card-img-top" src="{{ asset('assets/images/informations/edar-01.png') }}" alt="Gambar Kategori" />
                     <div class="card-body text-center py-3"><h6 class="card-title mb-0">Izin Edar</h6></div>
                 </a>
             </div>
-            <div class="col-md-6 col-lg-4">
+            <div class="col-md-4 col-lg-3">
                 <a class="card lift" href="{{ route('market') }}">
                     <img class="card-img-top" src="{{ asset('assets/images/informations/haki-01.png') }}" alt="Gambar Kategori" />
                     <div class="card-body text-center py-3"><h6 class="card-title mb-0">Haki</h6></div>
