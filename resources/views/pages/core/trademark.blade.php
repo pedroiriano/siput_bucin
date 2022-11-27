6873f6aeb0f0bf5d01c03bf07c77f1114023a72f
ipiyuza <pdzeus83aw@gmail.com>
2022-11-27 16:05:21 +0700

    Haki Page

--- resources/views/pages/core/trademark.blade.php
+++ resources/views/pages/core/trademark.blade.php
@@ -33,46 +33,32 @@
                 <p class="mb-0">Jadi dapat disimpulkan bahwa HAKI adalah hak untuk menikmati secara ekonomis hasil dari  suatu kreativitas intelektual. Objek yang diatur dalam kekayaan intelektual berupa karya yang dihasilkan oleh kemampuan intelektual Manusia.</p>
                 <hr class="my-5" />
                 <h2 class="mb-4">Cara pengurusan Hak Kekayaan Intelektual</h2>
-                <p>
-                    Pelaku/Penggiat usaha dapat melakukan registrasi online melalui situs resmi BPOM <a href="https://e-reg.pom.go.id/" target="_blank">https://e-reg.pom.go.id/</a> dengan langkah, sebagai berikut:
-                    <ol>
-                        <li>Isi form registrasi Perusahaan, dengan mengisi dan mengunggah data, dokumen-dokumen milik usaha serta melampirkan dokumen pendukung lainnya yang diminta oleh pihak BPOM</li>
-                        <li>Tunggu evaluasi dan verifikasi dari Petugas. Setelahnya, Pendaftar akan mendapat email dari BPOM yang berisi User ID dan Password untuk mengakses kembali situs BPOM. Lalu, login menggunakan User ID dan Password tersebut</li>
-                        <li>Masukkan data dan unggah dokumen pendukung yang diminta. Kemudian, klik proses</li>
-                        <li>Tunggu penerbitan surat perintah bayar. Setelah terbit, silakan bayar sesuai surat perintah bayar yang telah diperoleh</li>
-                        <li>Tunggu evaluasi verifikasi dan validasi dari Petugas BPOM</li>
-                        <li>Setelahnya, penerbitan Izin Edar. Jika data dianggap valid, Pendaftar akan mendapatkan Nomor Izin Edar secara elektronik</li>
-                    </ol>
-                </p>
-                <div class="accordion shadow mb-5" id="accordionPirt01">
+                <div class="accordion shadow mb-5" id="accordionHaki01">
                     <div class="accordion-item">
                         <div class="d-flex align-items-center justify-content-between px-4 py-5">
                             <div class="me-3">
                                 <h4 class="mb-0">Tahapan</h4>
-                                <p class="card-text text-gray-500">Rincian setiap langkah pengurusan Hak Kekayaan Intelektual berdasarkan aturan Kementerian Hukum dan HAM RI <a href="https://www.dgip.go.id/menu-utama/merek/syarat-prosedur" target="_blank">dapat dilihat di sini/</a></p>
+                                <p class="card-text text-gray-500">Rincian setiap langkah pengurusan Hak Kekayaan Intelektual berdasarkan<a href="https://www.dgip.go.id/menu-utama/merek/syarat-prosedur" target="_blank">aturan Kementerian Hukum dan HAM RI</a></p>
                             </div>
                         </div>
                     </div>
                     <div class="accordion-item">
-                        <h5 class="accordion-header" id="headingPirt01"><button class="accordion-button p-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePirt01" aria-expanded="true" aria-controls="collapsePirt01">Datang ke Dinas Kesehatan (DINKES)</button></h5>
-                        <div class="accordion-collapse collapse show" id="collapsePirt01" aria-labelledby="headingPirt01" data-bs-parent="#accordionPirt01">
+                        <h5 class="accordion-header" id="headingHaki01"><button class="accordion-button p-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseHaki01" aria-expanded="true" aria-controls="collapseHaki01">Syarat</button></h5>
+                        <div class="accordion-collapse collapse show" id="collapseHaki01" aria-labelledby="headingHaki01" data-bs-parent="#accordionHaki01">
                             <div class="accordion-body p-4">
-                                Dengan membawa syarat sebagai berikut:
+                                Berikut ini adalah persyaratan yang harus disiapkan:
                                 <ol>
-                                    <li>Mengisi formulir pendaftaran (nama perusahaan, alamat, nama pemilik, alamat pemilik, nama produk, jenis produk, proses pembuatan, jenis kemasan, mencantumkan komposisi, dan desain kemasan)</li>
-                                    <li>Fotokopi KTP</li>
-                                    <li>Pas foto 3 x 4 sebanyak 2 lembar</li>
-                                    <li>Surat keterangan usaha dari Puskesmas (yang keluar setelah petugas Puskesmas meninjau lokasi usaha)</li>
-                                    <li>Denah lokasi usaha</li>
-                                    <li>Draft label produk yang terdapat dalam kemasan (nama produk, merek, produsen, alamat produsen, komposisi, berat bersih, tanggal kedaluwarsa, kode produksi, nomor PIRT)</li>
-                                    <li>Stempel usaha.</li>
+                                    <li>Etiket/Label Merek</li>
+                                    <li>Tanda Tangan Pemohon</li>
+                                    <li>Surat Rekomendasi UKM Binaan atau Surat Keterangan UKM Binaan Dinas (Asli) - Untuk Pemohon Usaha Mikro dan Usaha Kecil <a href="https://dgip.go.id/uploads/article_file/file/6cf25c52e52779b4dcda63e7f90ea474.pdf" target="_blank">(Unduh Contoh Surat UMK)</a></li>
+                                    <li>Surat Pernyataan UMK Bermaterai - Untuk Pemohon Usaha Mikro dan Usaha Kecil <a href="https://dgip.go.id/uploads/media/file/f8b8967e247d1807bc75eb0cfce4ff78.docx" target="_blank">(Unduh Contoh Surat Pernyataan UMK)</a></li>
                                 </ol>
                             </div>
                         </div>
                     </div>
                     <div class="accordion-item">
                         <h5 class="accordion-header" id="headingPirt02"><button class="accordion-button p-4 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePirt02" aria-expanded="false" aria-controls="collapsePirt02">Mengikuti Penyuluhan Keamanan Pangan</button></h5>
-                        <div class="accordion-collapse collapse" id="collapsePirt02" aria-labelledby="headingPirt02" data-bs-parent="#accordionPirt01">
+                        <div class="accordion-collapse collapse" id="collapsePirt02" aria-labelledby="headingPirt02" data-bs-parent="#accordionHaki01">
                             <div class="accordion-body p-4">
                                 Penyuluhan ini bersifat kolektif, yaitu dilaksanakan jika peserta sudah memenuhi kuota yang ditentukan. Adapun, materi penyuluhan yang diberikan, antara lain:
                                 <ol>
@@ -91,7 +77,7 @@
                     </div>
                     <div class="accordion-item">
                         <h5 class="accordion-header" id="headingPirt03"><button class="accordion-button p-4 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePirt03" aria-expanded="false" aria-controls="collapsePirt03">Survei Lapangan oleh Petugas Puskesmas</button></h5>
-                        <div class="accordion-collapse collapse" id="collapsePirt03" aria-labelledby="headingPirt03" data-bs-parent="#accordionPirt01">
+                        <div class="accordion-collapse collapse" id="collapsePirt03" aria-labelledby="headingPirt03" data-bs-parent="#accordionHaki01">
                             <div class="accordion-body p-4">
                                 Survei dan pengecekan ke lokasi usaha tersebut bertujuan untuk melihat proses produksi serta bahan-bahan yang dipergunakan. Jika memang dibutuhkan, sampel pangan pun akan diteliti dengan uji laboratorium. Adapun beberapa aspek yang disurvei, antara lain:
                                 <ol>
@@ -109,7 +95,7 @@
                     </div>
                     <div class="accordion-item">
                         <h5 class="accordion-header" id="headingPirt04"><button class="accordion-button p-4 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePirt04" aria-expanded="false" aria-controls="collapsePirt04">Mengambil Sertifikat Produksi Pangan Industri Rumah Tangga</button></h5>
-                        <div class="accordion-collapse collapse" id="collapsePirt04" aria-labelledby="headingPirt04" data-bs-parent="#accordionPirt01">
+                        <div class="accordion-collapse collapse" id="collapsePirt04" aria-labelledby="headingPirt04" data-bs-parent="#accordionHaki01">
                             <div class="accordion-body p-4">
                                 Jika semua tahap yang dilewati sesuai prosedur, PIRT akan keluar dalam waktu kurang lebih 2 minggu. Peserta pengajuan PIRT akan mendapatkan dua sertifikat, yaitu sertifikat penyuluhan dan sertifikat PIRT. Dari situ, otomatis produk usahamu sudah terdaftar secara legal pada Dinas Kesehatan dan wajib diperbaharui setelah masa berlakunya sudah habis (3 atau 5 tahun).
                             </div>
